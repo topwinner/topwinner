b057cd1f375810d9ad7f054be060c4616bcd0b33
Timo Jiang <timo.jiang@oocl.com>
2012-08-21 20:16:52 +0800

    Change Home Ui Style

--- sites/optcalendar/urls.py
+++ sites/optcalendar/urls.py
@@ -2,9 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 from optcalendar import views
 
 urlpatterns = patterns('',
-<<<<<<< HEAD
     url(r'^$', views.index, name='optcalendar_index'),
-=======
-    url(r'^$', views.index, name='optcalendar'),
->>>>>>> origin/master
 )
